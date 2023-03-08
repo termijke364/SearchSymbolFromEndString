882dbb9cee1ad3b509c2087f385e38494200a02a
Nikita Pepelyaev <termi364@mail.ru>
2023-03-08 11:31:03 +0400

    Add project files.

---
+++ SearchSymbolFromEndString/SearchSymbolFromEndString.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int GetValue()
+{
+	int value;
+	cin >> value;
+
+	while (value < 0)
+	{
+		cout << "Введите значение больше 0!" << endl;
+		cin >> value;
+	}
+	return value;
+}
+
+int main()
+{
+	setlocale(0, "");
+
+	int const size = 50;
+	char buffer[size];
+	cout << "Введите текст: " << endl;
+	gets_s(buffer);
+
+	cout << "Введите искомый символ: " << endl;
+	char letter = getchar();
+	cin.ignore();
+
+	for (int i = size; i >= 1; i--)
+	{
+		if (*(buffer + i) == letter)
+		{
+			cout << "Индекс последнего совпадения найденого символа: " << i + 1 << endl;
+			break;
+		}
+	}
+
+	return 0;
+}
